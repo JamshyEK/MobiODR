3aa4962c1bf4502a3e9768ab2c462d40a1f772ff
JamshyEK <ekmjamshy@gmail.com>
2020-02-21 11:11:07 +0530

    All Test all cleared

--- app/src/main/java/com/example/mobiodr/LoginActivity.java
+++ app/src/main/java/com/example/mobiodr/LoginActivity.java
@@ -7,13 +7,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText ed1;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
 
-        ed1=findViewById(R.id.username);
-        ed1.setHint("test1");
     }
 
 }
