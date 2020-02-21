f49694ff325872c637b00f008004519bce6b391c
JamshyEK <ekmjamshy@gmail.com>
2020-02-21 10:14:25 +0530

    test in username hint

--- app/src/main/java/com/example/mobiodr/LoginActivity.java
+++ app/src/main/java/com/example/mobiodr/LoginActivity.java
@@ -1,14 +1,19 @@
 package com.example.mobiodr;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText ed1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
 
+        ed1=findViewById(R.id.username);
+        ed1.setHint("test");
     }
 
 }
